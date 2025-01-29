d1d65bace4acab06f3e7278e3eeef3353543309b
Nicholas Ross <nickross4444@gmail.com>
2025-01-29 00:45:21 -0800

    Fixed summon shattering

--- Assets/Scripts/JiggleFollow.cs
+++ Assets/Scripts/JiggleFollow.cs
@@ -6,11 +6,9 @@ public class JiggleFollow : MonoBehaviour
     [SerializeField][Tooltip("The transform this object will follow")] Transform target;
     [SerializeField] float followSpeed = 30f;
     [SerializeField] float damping = 6f;
-    [SerializeField] float torqueSpeed = 1f;
-    [SerializeField] float rotationDamping = 10f;
+    [SerializeField] float rotationSpeed = 10f;
 
     private Vector3 velocity = Vector3.zero;
-    private Quaternion angularVelocity = Quaternion.identity;
 
     HandGrabInteractable handGrabInteractable;
 
@@ -30,24 +28,8 @@ public class JiggleFollow : MonoBehaviour
             transform.position += velocity * Time.fixedDeltaTime;
             velocity = Vector3.Lerp(velocity, Vector3.zero, damping * Time.fixedDeltaTime);
 
-            // Rotation physics
-            Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
-            float angle;
-            Vector3 axis;
-            rotationDifference.ToAngleAxis(out angle, out axis);
-
-            // Avoid super large angles
-            if (angle > 180) angle -= 360;
-
-            // Apply torque based on angle difference
-            if (Mathf.Abs(angle) > 0.01f)
-            {
-                angularVelocity = Quaternion.AngleAxis(angle * torqueSpeed * Time.fixedDeltaTime, axis) * angularVelocity;
-            }
-
-            // Apply angular velocity with damping
-            transform.rotation = transform.rotation * angularVelocity;
-            angularVelocity = Quaternion.Slerp(angularVelocity, Quaternion.identity, rotationDamping * Time.fixedDeltaTime);
+            // Simple rotation following
+            transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationSpeed * Time.fixedDeltaTime);
         }
     }
     public void SetTarget(Transform newTarget)
